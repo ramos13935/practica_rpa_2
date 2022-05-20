be5d313963f073ff9e084724932e4f80fb7e1514
Carlos Ramos <cramos@devcenter.es>
2022-05-20 12:44:01 +0200

    Whatsapp Bot Funcional 1.0 20/05/2022 12:40

--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -21,28 +21,73 @@ def start(lista_movimientos):
     saludar(driver)
     correo = esperar_correo(driver)
     while not salir:
-        menu(driver)
-        opcion = recoger_opcion(driver)
-        print("Opción start:", opcion)
-        match opcion:
-            case "1":
-                print("entro a opción 1")
-                mostrar_movimientos(driver, correo, lista_movimientos)
-            case "2":
-                pass
-            case "3":
-                pass
-            case "0":
-                salir = True
+        menu(driver, correo, lista_movimientos)
+        opcion = recoger_ultimo_mensaje(driver)
+        if opcion == "1":
+            submenu_movimientos(driver, correo, lista_movimientos)
+        elif opcion == "2":
+            pass
+        elif opcion == "3":
+            enviar_mensaje(driver, ["Puede llamar al 12345679 (L-V, 8:00-21:00) para obtener más información"])
+            salir = True
+
+        elif opcion == "0":
+            salir = True
+        else:
+            enviar_mensaje(driver, ["Por favor escriba una de las opciones válidas"])
+
     mensaje_despedida = "¡Nos vemos pronto!"
-    enviar_mensaje(driver, mensaje_despedida)
+    enviar_mensaje(driver, [mensaje_despedida])
 
 
-def enviar_mensaje(driver: webdriver, mensaje: str):
+def menu(driver: webdriver, correo, lista_movimientos):
+    nombre = "Bienbenido/a " + get_nombre(correo, lista_movimientos) + " las opciones disponibles son:"
+    op1 = "*1.* Movimientos"
+    op2 = "*2.* ..."
+    op3 = "*3.* Hablar con un agente"
+    op0 = "*0.* Finalizar"
+    enviar_mensaje(driver, [nombre, op1, op2, op3, op0])
+
+
+def submenu_movimientos(driver, correo, lista_movimientos):
+    tit = "*MOVIMIENTOS*"
+    op1 = "*1.* Buscar movimiento"
+    op2 = "*2.* Mostrar movimientos "
+    op0 = "*0.* Volver al menú principal"
+    flag = True
+    while flag:
+        enviar_mensaje(driver, [tit, op1, op2, op0])
+        resp = recoger_ultimo_mensaje(driver)
+        if resp == "1":
+            mostrar_movimiento(driver, correo, lista_movimientos)
+        elif resp == "2":
+            mostrar_movimientos(driver, correo, lista_movimientos)
+        elif resp == "0":
+            flag = False
+        else:
+            enviar_mensaje(driver, ["No es una opción valida, escriba una de las opciones "])
+
+
+def escribir_emoticono(driver: webdriver, emoticono: str):
     barra_escribir_mensaje = driver.find_element(by=By.XPATH,
                                                  value="//*[@id='main']/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]")
-    barra_escribir_mensaje.send_keys(mensaje, Keys.ENTER)
-    print(mensaje)
+    barra_escribir_mensaje.send_keys(emoticono, Keys.ENTER)
+
+
+def enviar_mensaje(driver: webdriver, mensajes: list):
+    barra_escribir_mensaje = driver.find_element(by=By.XPATH,
+                                                 value="//*[@id='main']/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[2]")
+    for mensaje in mensajes:
+        if mensaje[0] == ":":
+            barra_escribir_mensaje.send_keys(mensaje, Keys.ENTER)
+            barra_escribir_mensaje.send_keys("", Keys.SHIFT + Keys.ENTER)
+        else:
+            barra_escribir_mensaje.send_keys(mensaje, Keys.SHIFT + Keys.ENTER)
+
+    btn_enviar = driver.find_element_by_xpath(
+        '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[2]/button/span[@data-testid="send"]')
+    btn_enviar.click()
+    time.sleep(1)
 
 
 def abrir_navegador():
@@ -51,7 +96,7 @@ def abrir_navegador():
     driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
     driver.maximize_window()
     driver.get("https://web.whatsapp.com/")
-    time.sleep(6)
+    time.sleep(5)
     return driver
 
 
@@ -61,53 +106,86 @@ def abrir_chat(driver: webdriver, nombre_chat: str):
         if chat.text == nombre_chat:
             chat.click()
             break
+    time.sleep(2)
 
 
 def esperar_correo(driver: webdriver):
-    check = False
+    flag = True
     correo = ""
-    time.sleep(2)
-    while not check:
-        mensajes_usu = driver.find_elements(by=By.XPATH,
-                                            value='//*[@id="main"]/div[3]/div/div[2]/div/div/div/div/div/div/div/span[@class="i0jNr selectable-text copyable-text"]/span')
-        if "@" in mensajes_usu[-1].text:
-            correo = mensajes_usu[-1].text
-            check = True
+    while flag:
+        correo = recoger_ultimo_mensaje(driver)
+        if "@" in correo:
+            flag = False
+        else:
+            enviar_mensaje(driver, ["Escriba un *correo electrónico* válido por favor."])
 
     return correo
 
 
 def saludar(driver: webdriver):
-    mensaje_saludo = "Hola soy Jarvis, tu asistente.\nPara que podamos empezar, ¿podría indicarme su *correo electrónico*?"
-    enviar_mensaje(driver, mensaje_saludo)
+    l1 = "Hola, soy Jarvis, tu asistente."
+    em = ":saludar"
+    l2 = "Para que podamos empezar, ¿podría indicarme su *correo electrónico*?"
+    enviar_mensaje(driver, [l1, em, l2])
 
 
-def menu(driver: webdriver):
-    opciones_menu = "*1.* Consultar Movimientos\n*2.* ...\n*3.* Hablar con un agente\n*0.* Finalizar"
-    enviar_mensaje(driver, opciones_menu)
-
-
-def recoger_opcion(driver: webdriver):
+def recoger_ultimo_mensaje(driver: webdriver):
     check = False
-    opcion = ""
+    ultimo_mensaje = ""
+    value_mensajes = '//*[@id="main"]/div[3]/div/div[2]/div/div/div/div/div/div/div/span[@class="i0jNr selectable-text copyable-text"]/span'
+    contador_mesajes = len(driver.find_elements(by=By.XPATH, value=value_mensajes))
     while not check:
         mensajes_usu = driver.find_elements(by=By.XPATH,
-                                            value='//*[@id="main"]/div[3]/div/div[2]/div/div/div/div/div/div/div/span[@class="i0jNr selectable-text copyable-text"]/span')
+                                            value=value_mensajes)
 
-        if "0. Finalizar" not in mensajes_usu[-1].text:
-            opcion = mensajes_usu[-1].text
+        if len(mensajes_usu) > contador_mesajes:
+            ultimo_mensaje = mensajes_usu[-1].text
             check = True
 
-    return opcion
+        # time.sleep(0.5)
+    return ultimo_mensaje
+
+
+def mostrar_movimiento(driver, correo, lista_movimientos):
+    flag = True
+    while flag:
+        enviar_mensaje(driver, ["Escribe el código del movimiento que quiere revisar:"])
+        cod_mov = recoger_ultimo_mensaje(driver)
+        mensaje = ["*_Datos del movimiento:*_"]
+        permiso = True
+        for movimiento in lista_movimientos:
+            if str(movimiento.codigo_operacion) == cod_mov and correo == movimiento.email:
+                mensaje.append(movimiento.__str__())
+                break
+            elif str(movimiento.codigo_operacion) == cod_mov and correo != movimiento.email:
+                enviar_mensaje(driver, ["No tienes permiso para ver ese movimiento"])
+                permiso = False
+                break
+
+        if permiso and len(mensaje) > 1:
+            enviar_mensaje(driver, mensaje)
+        elif permiso:
+            enviar_mensaje(driver, ["Código de operación no encontrado"])
+
+        enviar_mensaje(driver, ["¿Quieres buscar otro movimiento? (Si/No)"])
+        resp = recoger_ultimo_mensaje(driver)
+        if "Si" not in resp and "si" not in resp:
+            flag = False
 
 
 def mostrar_movimientos(driver: webdriver, correo: str, lista_movimientos):
-    mensaje = ""
-    print("Mostrar movimientos: ", correo)
+    mensaje = ["_*Todos los movimientos:*_"]
     for movimiento in lista_movimientos:
         if movimiento.email == correo:
-            mensaje += movimiento.__str__() + "\n"
-
-    print("Movimientos totales:", mensaje)
+            mensaje.append(movimiento.__str__())
 
     enviar_mensaje(driver, mensaje)
+
+
+def get_nombre(correo, lista_movimientos):
+    nombre = ""
+    for movimiento in lista_movimientos:
+
+        if movimiento.email == correo:
+            nombre = movimiento.nombre
+    return nombre
